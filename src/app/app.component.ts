6ea395e005d7370fabdb75074d4042b7fa2685f3
Shankar <212604198@logon.ds.ge.com>
2019-10-22 10:41:43 +0530

    Changes for microapp to receive the messages from iframe shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'microapp1';
+
+  displayMessage (evt) {
+    console.log("Receiving message from Mural Shell");
+    console.log(evt);
+    var message;
+    /*
+    if (evt.origin !== "https://robertnyman.com") {
+      message = "You are not worthy";
+    } 
+    else { */
+      message = "I got " + evt.data + " from " + evt.origin;
+    //}
+    document.getElementById("message-div").innerHTML = message;
+    this.title = evt.data;
+  }
+  
+  ngOnInit() {
+    console.log("In ngonit of appComponents.ts of microapp1");
+    if (window.addEventListener) {
+      // For standards-compliant web browsers
+      window.addEventListener("message", this.displayMessage, false);
+    }
+    /*
+    else {
+      window.attachEvent("onmessage", displayMessage);
+    } */
+  }
 }
